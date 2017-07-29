881abb8444cff7bedd416abcffc1e0618fb08d1a
LuoShunXiang <marishunxiang@gmail.com>
2017-07-29 19:06:40 +0800

    message item

--- app/src/main/java/youlin/xinhua/com/youlin/widget/ChatInputView.java
+++ app/src/main/java/youlin/xinhua/com/youlin/widget/ChatInputView.java
@@ -487,7 +487,7 @@ public class ChatInputView extends LinearLayout
     mPhotoBtn.setChecked(false);
 
     mRecordVoiceRl.setVisibility(VISIBLE);
-    mRecordContentLl.setVisibility(VISIBLE);
+    //mRecordContentLl.setVisibility(VISIBLE);
 
     dismissPhotoLayout();
     dimissEmojLayout();
---
+++ app/src/main/java/youlin/xinhua/com/youlin/widget/ChatMessageList.java
@@ -0,0 +1,13 @@
+package youlin.xinhua.com.youlin.widget;
+
+/**
+ * <pre>
+ * desc   : Todo
+ * author : 罗顺翔
+ * time   : 2017-07-29 10:59
+ * version: 1.0
+ * </pre>
+ */
+
+public class ChatMessageList {
+}

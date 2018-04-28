908f783f90b983cdbae9b49cbe4218c01bde634f
DESKTOP-I6UC1UM\Administrator <Marishunxiang@gmail.com>
2018-04-28 17:04:10 +0800

    build

--- app/src/test/java/youlin/xinhua/com/youlin/ExampleUnitTest.java
+++ app/src/test/java/youlin/xinhua/com/youlin/ExampleUnitTest.java
@@ -13,4 +13,23 @@ public class ExampleUnitTest {
   @Test public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
   }
+
+  @Test
+  public void test0() {
+    int[] array = {5, 8, 6, 4, 7, 1, 2 , 6};
+
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[i] > array[j]) {
+          int temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " , ");
+    }
+  }
 }
